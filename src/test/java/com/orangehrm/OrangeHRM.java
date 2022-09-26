5d21ce4831edacf4a16e0955cbb418ef79862e94
Neha Khare <patilneha805@gmail.com>
2022-09-26 12:53:35 +0530

    initial code commit

--- src/test/java/com/orangehrm/OrangeHRM.java
+++ src/test/java/com/orangehrm/OrangeHRM.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Test
 public class OrangeHRM {
 
 	WebDriver driver;
@@ -20,8 +21,11 @@ public class OrangeHRM {
 		driver = new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		implicitWait(3);
+		driver.manage().window().maximize();
+		implicitWait(3);
 	}
 
+	@Test
 	public void aLogincredentials() throws Exception {
 		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
 		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
@@ -32,77 +36,76 @@ public class OrangeHRM {
 	}
 
 	@Test
-	public void baAdmin() throws Exception {
+	public void admin() throws Exception {
 		aLogincredentials();
 		driver.findElement(By.xpath("//a[contains(@href, '/web/index.php/admin/viewAdminModule')]")).click();
 		implicitWait(3);
-		// driver.findElement(By.xpath("//label[contains(text(),'Username']"));
-		driver.findElement(By.xpath("//div[@data-v-2fe357a6]//input[@data-v-844e87dc]")).sendKeys("Charlie.Carter");
-		implicitWait(3);
-
-		driver.findElement(By.xpath("//div[@data-v-4acd865c]")).click();
-		implicitWait(3);
-		driver.findElement(By.xpath("//div[@data-v-bdd98d14]//input[@data-v-7c56a434]")).sendKeys("Charlie  Carter");
-		implicitWait(3);
-		driver.findElement(
-				By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']"))
-				.submit();
-		implicitWait(3);
 	}
 
-	public void bbUserManagment() throws Exception {
-		baAdmin();
+	@Test
+	public void adminUserManagement() throws Exception {
+		admin();
 		driver.findElement(By.xpath("//span[contains(text(), 'User Management ')]")).click();
 		implicitWait(3);
 		Thread.sleep(2000);
 		driver.findElement(By.linkText("Users")).click();
 		implicitWait(3);
+		driver.findElement(By.xpath("//div[@data-v-2fe357a6]//input[@data-v-844e87dc]")).sendKeys("Charlie.Carter");
+		implicitWait(3);
+		driver.findElement(By.xpath("//div[@data-v-4acd865c]")).click();
+		implicitWait(3);
+		driver.findElement(By.xpath("//div[@data-v-bdd98d14]//input[@data-v-7c56a434]")).sendKeys("Charlie  Carter");
+		implicitWait(3);
+		// driver.findElement(
+		// By.xpath("//button[@class='oxd-button oxd-button--medium
+		// oxd-button--secondary orangehrm-left-space']"))
+		// .submit();
+		driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")).click();
+		implicitWait(3);
 	}
 
-	public void bcJob() throws Exception {
-		baAdmin();
+	@Test
+	public void adminJobJobTitles() throws Exception {
+		admin();
 		driver.findElement(By.xpath("//span[contains(text(), 'Job ')]")).click();
 		implicitWait(3);
 		Thread.sleep(2000);
 		driver.findElement(By.linkText("Job Titles")).click();
 		implicitWait(3);
 		// driver.findElements(By.xpath("//span[@data-v-5ba85061]")).get(13).click();
-		driver.findElements(By.xpath("//span[@data-v-5ba85061]")).get(13).click();
+		// implicitWait(3);
+		driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")).click();
 		implicitWait(3);
-
+		driver.findElement(By.xpath("//div[@data-v-2fe357a6]//input[@data-v-844e87dc]")).sendKeys("IT Manager");
+		implicitWait(3);
 	}
 
-	public void bdAdd() throws Exception {
-		baAdmin();
+	@Test
+	public void adminJobPayGrades() throws Exception {
+		admin();
 		driver.findElement(By.xpath("//span[contains(text(), 'Job ')]")).click();
 		implicitWait(3);
-		driver.findElement(By.linkText("Job Titles")).click();
+		driver.findElement(By.linkText("Pay Grades")).click();
 		implicitWait(3);
-		driver.findElement(By.xpath("//button[@data-v-7e88b27e]")).click();
+		//driver.findElements(By.xpath("//span[@data-v-5ba85061]")).get(2).click();
 		implicitWait(3);
-
+		driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")).click();
+		implicitWait(3);
+		driver.findElement(By.xpath("//div[@data-v-2fe357a6]//input[@data-v-844e87dc]")).sendKeys("Anny");
+		implicitWait(3);
 	}
 
-	public void caMyInfo() throws Exception {
-		aLogincredentials();
-		driver.findElements(By.xpath("//span[@data-v-4de0f1ff)]")).get(6).click();
-		implicitWait(3);
 
-	}
-
-	/*
-	 * public void fOrganization() throws Exception { bAdmin();
-	 * driver.findElement(By.xpath("//span[contains(text(), 'Organization ')]")).
-	 * click(); }
-	 */
+	@Test
 	private void implicitWait(int time) {
 		driver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
 	}
 
-	@AfterMethod
-	public void destroy() throws Exception {
-		Thread.sleep(2000);
-		driver.quit();
-	}
-
+	
+	  @AfterMethod 
+	  public void destroy() throws Exception 
+	  { 
+		  implicitWait(3);
+	      driver.quit(); }
+	 
 }
